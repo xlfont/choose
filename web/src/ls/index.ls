c1b8a6e17c7455b4005257b2651cc4f8d6ee3675
Kirby Wu <tkirby@gmail.com>
2021-10-24 17:06:47 +0800

    upgrade modules

--- web/src/ls/index.ls
+++ web/src/ls/index.ls
@@ -1,22 +1,31 @@
+<-(->it.apply {}) _
+
 textarea = document.querySelector \#demo-textarea
-base = "https://xlfont.github.io/sample-set"
+base = "assets/fonts" #"https://xlfont.github.io/sample-set"
 
 i18next.init supportedLng: <[en zh-TW]>, fallbackLng: \zh-TW
-  .then ->
+  .then ~>
 
     modal-chooser = new xfc {
       root: '.ldcv .xfc'
       #meta-root: 'assets/fonts/meta'
       #font-root: 'assets/fonts/links'
-      meta-root: "#base/meta"
-      font-root: "#base/links"
+      meta: "#base/meta"
+      links: "#base/links"
       i18n: i18next
     }
+    @view = new ldview do
+      root: document.body
+      action: click: choose: ~> ldcv.toggle!
+
+
+
 
     modal-chooser.init!
-    window.ldcv = ldcv = new ldCover root: '.ldcv'
-    modal-chooser.on \choose, (f) ->
-      window.ldcv.toggle false
+    @ldcv = ldcv = new ldcover root: '.ldcv'
+    @ldcv.on \toggle.on, -> modal-chooser.render!
+    modal-chooser.on \choose, (f) ~>
+      @ldcv.toggle false
       if !f => return
       textarea.style.fontFamily = f.name
       f.sync textarea.value
--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,27 +1,44 @@
-var textarea, base;
-textarea = document.querySelector('#demo-textarea');
-base = "https://xlfont.github.io/sample-set";
-i18next.init({
-  supportedLng: ['en', 'zh-TW'],
-  fallbackLng: 'zh-TW'
-}).then(function(){
-  var modalChooser, ldcv;
-  modalChooser = new xfc({
-    root: '.ldcv .xfc',
-    metaRoot: base + "/meta",
-    fontRoot: base + "/links",
-    i18n: i18next
-  });
-  modalChooser.init();
-  window.ldcv = ldcv = new ldCover({
-    root: '.ldcv'
-  });
-  return modalChooser.on('choose', function(f){
-    window.ldcv.toggle(false);
-    if (!f) {
-      return;
-    }
-    textarea.style.fontFamily = f.name;
-    return f.sync(textarea.value);
+(function(it){
+  return it.apply({});
+})(function(){
+  var textarea, base, this$ = this;
+  textarea = document.querySelector('#demo-textarea');
+  base = "assets/fonts";
+  return i18next.init({
+    supportedLng: ['en', 'zh-TW'],
+    fallbackLng: 'zh-TW'
+  }).then(function(){
+    var modalChooser, ldcv;
+    modalChooser = new xfc({
+      root: '.ldcv .xfc',
+      meta: base + "/meta",
+      links: base + "/links",
+      i18n: i18next
+    });
+    this$.view = new ldview({
+      root: document.body,
+      action: {
+        click: {
+          choose: function(){
+            return ldcv.toggle();
+          }
+        }
+      }
+    });
+    modalChooser.init();
+    this$.ldcv = ldcv = new ldcover({
+      root: '.ldcv'
+    });
+    this$.ldcv.on('toggle.on', function(){
+      return modalChooser.render();
+    });
+    return modalChooser.on('choose', function(f){
+      this$.ldcv.toggle(false);
+      if (!f) {
+        return;
+      }
+      textarea.style.fontFamily = f.name;
+      return f.sync(textarea.value);
+    });
   });
 });
